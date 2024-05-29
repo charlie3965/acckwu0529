5cf373374467f9f3922c5ea59f2a93d45bc67237
charlie3965 <charlie39653965@gmail.com>
2024-05-29 20:52:16 +0900

    changed output sentence

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("Hello ACCKWU! 으헤헤헤헤헤")
+    res.send("Hello ACCKWU! 오후 8시 51분")
 })
 
 app.listen(5000)
